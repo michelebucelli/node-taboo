1896176faa193f415f7473efc311b9d26b4ff6ef
Michele Bucelli <michele.bucelli@mail.polimi.it>
2019-03-17 22:21:09 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,324 @@
+// Game data ///////////////////////////////////////////////////////////////////
+
+// Game deck (italian)
+const deck_it = [
+   [ "rimmel", "trucco", "ciglia", "cosmetico", "allungare", "De Gregori" ],
+   [ "apnea", "palombaro", "bombole", "immersione", "respirare", "sommozzatore" ],
+   [ "antracite", "nero", "colore", "carbone", "fossile", "grigio" ],
+   [ "apice", "cima", "punta", "vertice", "culmine", "massimo" ],
+   [ "arcipelgo", "Antille", "isole", "gruppo", "mare", "Eolie" ],
+   [ "arguto", "avveduto", "fine", "ingegnoso", "sottile", "osservazione" ],
+   [ "scoop", "notizia", "giornale", "rivista", "colpo", "sensazionale" ],
+   [ "argilla", "roccia", "porosa", "piedi", "creta", "ceramica" ],
+   [ "appello", "scuola", "presente", "assente", "corte", "esame" ],
+   [ "appartamento", "casa", "stanza", "affitto", "abitazione", "acquistare" ],
+   [ "arachide", "nocciolina", "americana", "superpippo", "tostato", "olio" ],
+   [ "aquila", "uccello", "rapace", "intelligente", "vista", "condor" ],
+   [ "semolino", "brodo", "minestra", "farina", "riso", "neonato" ],
+   [ "arnese", "attrezzo", "strumento", "lavoro", "mestiere", "utensile" ],
+   [ "arrotino", "affilare", "mestiere", "lama", "coltello", "forbice" ],
+   [ "semaforo", "fermarsi", "verde", "rosso", "arancione", "incrocio" ],
+   [ "soggiorno", "viaggio", "permanenza", "salone", "azienda", "obbligato" ],
+   [ "asciugamano", "tela", "spugna", "acqua", "bagno", "accappatoio" ],
+   [ "assieme", "con", "stare", "riunire", "gruppo", "tutti" ],
+   [ "assai", "tanto", "abbastanza", "te voglio bene", "molto", "quantità" ],
+   [ "attimo", "cogliere", "carpe diem", "fuggente", "istante", "aspettare" ],
+   [ "attico", "ultimo piano", "edificio", "casa", "sopra", "abitazione" ],
+   [ "auge", "essere in", "apice", "culmine", "successo", "fama" ],
+   [ "audience", "televisione", "ascolti", "Auditel", "share", "spettatori" ],
+   [ "aumentare", "crescere", "tasse", "incrementare", "maggiore", "quantità" ],
+   [ "Romeo", "gigli", "innamorato", "Giulietta", "Shakespeare", "Verona" ],
+   [ "australiano", "continente", "abitante", "Canguro", "isola", "Sidney" ]
+];
+
+// Game logic //////////////////////////////////////////////////////////////////
+
+// Cards are represented by means of arrays of words
+// The first word is the one to be guessed, the following ones are the taboo words
+
+// Game state
+const NOT_CONNECTED = -1;
+const WAITING_FOR_PLAYERS = 0;
+const INGAME_OTHER_PLAYER_TURN = 1;
+const INGAME_MY_TURN = 2;
+const INGAME_STARTING_MY_TURN = 3;
+const INGAME_STARTING_OTHER_PLAYER_TURN = 4;
+var state = WAITING_FOR_PLAYERS;
+
+// Teams
+const BLUE_TEAM = 0;
+const RED_TEAM = 1;
+
+// Game parameters
+var game_maxTime = 30000;
+var game_maxSkip = 3;
+
+// Match class. Contains all information pertaining to the current game
+var Match = function ( ) {
+   // Index of the client whose turn is the current
+   this.turn = 0;
+
+   // Timer [msec]
+   this.time = 0;
+   this.maxTime = game_maxTime;
+   this.timerHandle = -1;
+
+   // Scoring
+   this.score = [ 0, 0 ];
+
+   // Current card
+   this.card = [ "", "", "", "", "", "", "" ];
+
+   // Deck of choice
+   this.deck = deck_it;
+
+   // Past cards; we keep track of them to avoid drawing the same card repeatedly
+   this.history = [];
+
+   // Skip count
+   this.skips = 0;
+   this.maxSkips = game_maxSkip;
+
+   // Draw a new card and sends it to correct player
+   this.drawCard = function ( ) {
+      var idx = Math.floor(Math.random() * this.deck.length);
+      while ( this.history.includes(idx) )
+         idx = Math.floor(Math.random() * this.deck.length);
+
+      this.card = this.deck[idx];
+      this.history.push ( idx );
+
+      if ( this.history.length == this.deck.length ) {
+         console.log ( "[GAME] resetting deck" );
+         this.history.splice(0, this.history.length);
+      }
+
+      clients[this.turn].emit ( "card", this.card );
+
+      for ( let i = 0; i < clients.length; ++i )
+         if ( clients[i].team != clients[this.turn].team )
+            clients[i].emit ( "card", this.card );
+   }
+
+   // Pre-start: the player whose turn is about to start gets notified; we wait for him to
+   // actually start
+   this.preStartTurn = function ( ) {
+      console.log ( "[GAME] client " + this.turn + " turn is about to start" );
+
+      // Communicate states to clients
+      io.emit ( "teamTurn", clients[this.turn].team );
+      io.emit ( "score", this.score );
+      clients[this.turn].emit ( "state", INGAME_STARTING_MY_TURN );
+
+      this.skips = this.maxSkips;
+      clients[this.turn].emit ( "skips", this.skips );
+
+      for ( let i = 0; i < clients.length; ++i )
+         if ( i != this.turn )
+            clients[i].emit ( "state", INGAME_STARTING_OTHER_PLAYER_TURN );
+   }
+
+   // Starts a new turn
+   this.startTurn = function ( ) {
+      this.time = this.maxTime;
+
+      console.log ( "[GAME] client " + this.turn + " turn starts now (team " + clients[this.turn].team + ")" );
+
+      // Communicate states to clients
+      clients[this.turn].emit ( "state", INGAME_MY_TURN );
+      for ( let i = 0; i < clients.length; ++i )
+         if ( i != this.turn )
+            clients[i].emit ( "state", INGAME_OTHER_PLAYER_TURN );
+
+      this.drawCard();
+
+      this.timerHandler = setInterval ( (function() {
+         this.time -= 10;
+         io.emit ( "time", this.time );
+         if ( this.time <= 0 ) this.endTurn();
+      }).bind(this), 10 );
+   }
+
+   // End of turn
+   this.endTurn = function ( ) {
+      clearInterval ( this.timerHandler );
+      console.log ( "[GAME] client " + this.turn + " turn ends" );
+
+      this.turn++;
+      if ( this.turn >= clients.length ) this.turn = 0;
+
+      this.preStartTurn();
+   }
+
+   // Correct answer
+   this.correct = function ( ) {
+      this.score[clients[this.turn].team] += 1;
+      io.emit ( "score", this.score );
+      this.drawCard();
+   }
+
+   // Taboo word
+   this.taboo = function ( ) {
+      this.score[clients[this.turn].team] -= 1;
+      io.emit ( "score", this.score );
+      this.drawCard();
+   }
+
+   // Skip
+   this.skipCard = function ( ) {
+      if ( this.skips <= 0 ) return;
+
+      this.drawCard();
+      this.skips--;
+      clients[this.turn].emit ( "skips", this.skips );
+   }
+
+   // Reset the game
+   this.reset = function ( ) {
+      this.turn = 0;
+      this.score = [0, 0];
+      clearInterval ( this.timerHandle );
+
+      for ( let i = 0; i < clients.length; ++i )
+         clients[i].team = undefined;
+
+      io.emit ( "ready", false );
+      io.emit ( "team", undefined );
+      io.emit ( "state", WAITING_FOR_PLAYERS );
+   }
+
+   // Start the match
+   this.startMatch = function () {
+      // Reset game object
+      this.reset();
+
+      // Assigns clients to teams
+      for (let i = 0; i < clients.length; ++i ) {
+         clients[i].team = i % 2;
+         clients[i].emit ( "team", clients[i].team );
+      }
+
+      // Resets game object
+      this.preStartTurn();
+   }
+}
+
+var match = new Match();
+
+// Server logic ////////////////////////////////////////////////////////////////
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var clients = [];
+var lastId = 0;
+var readyCount = 0;
+
+// Map requests to files
+app.get('/',           function(req, res){ res.sendFile(__dirname + '/index.html'); });
+app.get('/index.html', function(req, res){ res.sendFile(__dirname + '/index.html'); });
+app.get('/script.js',  function(req, res){ res.sendFile(__dirname + '/script.js'); });
+
+// Handle player connections
+io.on('connection', function(socket) {
+   console.log("[SRVR] client " + lastId + " connected");
+
+   // Register the client
+   socket.id = lastId++;
+   clients.push ( socket );
+
+   // Set client state to not ready
+   socket.ready = false;
+
+   // Sets client team to undefined
+   socket.team = undefined;
+
+   // Send room state and misc info
+   socket.emit ( 'id', socket.id );
+   socket.emit ( 'state', state );
+   socket.emit ( 'ready', false );
+   socket.emit ( 'team', undefined );
+   io.emit ( 'connectedPlayers', clients.length );
+   io.emit ( 'readyCount', readyCount );
+
+   // Disconnection event
+   socket.on ( 'disconnect', function() {
+      // Remove client from list
+      for (let i = 0; i < clients.length; ++i) {
+         if ( clients[i].id == this.id ) {
+            clients.splice ( i, 1 );
+            break;
+         }
+      }
+
+      if ( this.ready ) readyCount--;
+
+      io.emit ( 'connectedPlayers', clients.length );
+      console.log("[SRVR] client " + this.id + " disconnected");
+
+      if ( clients.length == 0 ) {
+         console.log("[SRVR] no players left, resetting");
+         state = WAITING_FOR_PLAYERS;
+         match.reset();
+      }
+   } );
+
+   // Ready state toggle event
+   socket.on ( 'ready', function() {
+      this.ready = !this.ready;
+
+      if ( this.ready ) {
+         readyCount++;
+         console.log("[SRVR] client " + this.id + " is ready");
+      }
+      else {
+         readyCount--;
+         console.log("[SRVR] client " + this.id + " is no longer ready");
+      }
+
+      io.emit ( "readyCount", readyCount );
+      console.log("[SRVR] " + readyCount + "/" + clients.length + " clients are ready" );
+
+      // Check if all clients are ready and if so start the game
+      if ( readyCount == clients.length ) {
+         console.log("[SRVR] All clients ready, starting game");
+
+         // Setup and start the game
+         match.startMatch();
+      }
+   });
+
+   // Start turn event
+   socket.on ( 'startTurn', function() {
+      if ( this.id == clients[match.turn].id ) {
+         match.startTurn();
+      }
+   } );
+
+   // Correct answer
+   socket.on ( 'correct', function() {
+      if ( this.id == clients[match.turn].id ) {
+         match.correct();
+      }
+   } );
+
+   // Taboo word
+   socket.on ( 'taboo', function() {
+      if ( this.id == clients[match.turn].id ) {
+         match.taboo();
+      }
+   } );
+
+   // Skip
+   socket.on ( 'skip', function() {
+      if ( this.id == clients[match.turn].id ) {
+         match.skipCard();
+      }
+   } );
+});
+
+// Listen for HTTP requests
+http.listen(3000, function(){
+   console.log("[SRVR] Listening on 3000");
+} );
---
+++ script.js
@@ -0,0 +1,137 @@
+// Game logic //////////////////////////////////////////////////////////////////
+
+// Game state
+const NOT_CONNECTED = -1;
+const WAITING_FOR_PLAYERS = 0;
+const INGAME_OTHER_PLAYER_TURN = 1;
+const INGAME_MY_TURN = 2;
+const INGAME_STARTING_MY_TURN = 3;
+const INGAME_STARTING_OTHER_PLAYER_TURN = 4;
+var state = NOT_CONNECTED;
+
+var connectedPlayers = 0;
+
+// Update game state
+var updateUI = function ( ) {
+   $(".state").hide();
+
+   $("body").toggleClass("redTeam", team == RED_TEAM );
+   $("body").toggleClass("blueTeam", team == BLUE_TEAM );
+
+   if ( state == NOT_CONNECTED ) {
+   }
+
+   if ( state == WAITING_FOR_PLAYERS ) {
+      $("#waiting").show();
+      $(".connectedPlayersCounter").text(connectedPlayers);
+      $(".readyPlayersCounter").text(readyCount);
+      $("#readyButton").toggleClass("pressed", ready);
+   }
+
+   if ( state == INGAME_STARTING_MY_TURN ) {
+      $("#startingMyTurn").show();
+      $("#startingMyTurn #score #red").text(score[RED_TEAM]);
+      $("#startingMyTurn #score #blue").text(score[BLUE_TEAM]);
+   }
+
+   if ( state == INGAME_STARTING_OTHER_PLAYER_TURN ) {
+      $("#startingOtherTurn").show();
+      $("#startingOtherTurn #score #red").text(score[RED_TEAM]);
+      $("#startingOtherTurn #score #blue").text(score[BLUE_TEAM]);
+      $("#startingOtherTurn #heading").text ( teamTurn == RED_TEAM ? "Turno squadra rossa" : "Turno squadra blu" );
+   }
+
+   if ( state == INGAME_MY_TURN ) {
+      $("#myTurn").show();
+      $("#myTurn #timer").text(Math.floor(time / 100) / 10);
+      $("#myTurn #heading").text(card[0]);
+      $("#myTurn #score #red").text(score[RED_TEAM]);
+      $("#myTurn #score #blue").text(score[BLUE_TEAM]);
+      $("#skipCount").text(skips);
+
+      for ( let i = 1; i < card.length; ++i )
+         $("#myTurn #tabooWord" + i).text(card[i]);
+   }
+
+   if ( state == INGAME_OTHER_PLAYER_TURN ) {
+      $("#otherTurn").show();
+      $("#otherTurn #timer").text(Math.floor(time / 100) / 10);
+      $("#otherTurn #score #red").text(score[RED_TEAM]);
+      $("#otherTurn #score #blue").text(score[BLUE_TEAM]);
+      $("#otherTurn #heading").text ( teamTurn == RED_TEAM ? "Turno squadra rossa" : "Turno squadra blu" );
+
+      for ( let i = 1; i < card.length; ++i )
+         $("#otherTurn #tabooWord" + i).text(card[i]);
+   }
+};
+
+// Teams
+const BLUE_TEAM = 0;
+const RED_TEAM = 1;
+var team = undefined;
+var teamTurn = undefined;
+
+// Scoring
+var score = [ 0, 0 ];
+
+// Ready flag
+var ready = false;
+var readyCount = 0;
+
+// Timer
+var time = 0;
+
+// Current card
+var card = [ "", "", "", "", "", "" ];
+
+// Skip count
+var skips = 0;
+
+// Server logic ////////////////////////////////////////////////////////////////
+
+var socket = io();
+var id = -1;
+
+socket.on ( "id", function (msg) { id = msg; } );
+socket.on ( "state", function (msg) { state = msg; updateUI(); } );
+socket.on ( "connectedPlayers", function(msg) { connectedPlayers = msg; updateUI(); } );
+socket.on ( "ready", function(msg) { ready = msg; updateUI(); } );
+socket.on ( "readyCount", function(msg) { readyCount = msg; updateUI(); } );
+socket.on ( "team", function(msg) { team = msg; updateUI(); } );
+socket.on ( "teamTurn", function(msg) { teamTurn = msg; updateUI(); } );
+socket.on ( "time", function(msg) { time = msg; updateUI(); } );
+socket.on ( "card", function(msg) { card = msg; updateUI(); } );
+socket.on ( "score", function(msg) { score = msg; updateUI(); } );
+socket.on ( "skips", function(msg) { skips = msg; updateUI(); } );
+socket.on ( "disconnect", function() { state = NOT_CONNECTED; } );
+
+// HTML page logic /////////////////////////////////////////////////////////////
+
+var setup = function ( ) {
+   $("#readyButton").on("click", function() {
+      ready = !ready;
+      socket.emit("ready");
+      updateUI();
+   } );
+
+   $("#startTurnButton").on("click", function() {
+      socket.emit ( "startTurn" );
+   } );
+
+   $("#rightButton").on("click", function() {
+      socket.emit ( "correct" );
+   } );
+
+   $("#tabooButton").on("click", function() {
+      socket.emit ( "taboo" );
+   } );
+
+   $("#skipButton").on("click", function() {
+      socket.emit ( "skip" );
+   } );
+
+   $("#playerName").on("input", function() {
+      localStorage.playerName = $("#playerName").val();
+   } );
+   $("#playerName").val(localStorage.playerName);
+};
